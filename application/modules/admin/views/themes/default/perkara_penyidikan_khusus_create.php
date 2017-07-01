1124205e28dcd4aa73fb911ba61911e860ec7317
Tonny H. Soerojo <tonny@soerojo.com>
2017-07-01 20:43:23 +0700

    Update Form naik_dik_mum & sus

--- application/modules/admin/views/themes/default/perkara_penyidikan_khusus_create.php
+++ application/modules/admin/views/themes/default/perkara_penyidikan_khusus_create.php
@@ -33,228 +33,139 @@
                   <?php endif; ?>
               <!-- Content -->
                 <!-- /.box-header -->
-                  <div class="row"> 
-                    <div class="col-lg-6 col-md-6 col-sm-12">
-                      <h4>DATA KASUS</h4>
-                      <!-- Judul Kasus -->
-                      <div class="form-group">
-                        <label>Judul Kasus</label>
-                        <textarea class="form-control" rows="2" placeholder="Enter ..." disabled="disabled"></textarea>
-                      </div>
-                      <!-- Surat Perintah -->
-                      <div class="form-group">
-                        <label>Judul Surat</label>
-                        <select class="form-control" >
-                          <option selected="selected">Judul Surat</option>
-                          option disabled="disabled">SURAT PERINTAH PENYELIDIKAN</option>
-                          <option disabled="disabled">SURAT PERINTAH PENYIDIKAN UMUM</option>
-                         <!--< <option>SURAT PENETAPAN TERSANGKA</option>-->
-                          <option disabled="disabled">SURAT PERINTAH PENYIDIKAN KHUSUS</option>
-                          <option disabled="disabled">SURAT PERINTAH PENGHENTIAN PERKARA</option>
-                          <<!--option>SURAT PERINTAH PENAHANAN</option>-->
-                          <!--<option disabled="disabled">SURAT PERINTAH GELEDAH / SITA</option>
-                          <option>BERITA ACARA</option>-->
-                        </select>
-                      </div>
-                      <!-- No. Surat -->
-                      <div class="form-group">
-                        <label>No. Surat</label>
-                        <input type="text" class="form-control" placeholder="Enter ...">
-                      </div>
-                      <!-- /.form group -->
-                      <!-- Tanggal Surat -->
-                      <div class="form-group">
-                        <label>Tanggal</label>
-                        <div class="input-group date">
-                          <div class="input-group-addon">
-                            <i class="fa fa-calendar"></i>
-                          </div>
-                          <input type="text" class="form-control pull-right" id="datepicker">
-                        </div>
-                        <!-- /.input group -->
-                      </div>
-                      <!-- /.form group -->
-                       <!-- Lokasi -->
-                      <div class="form-group">
-                        <label>Lokasi Kejadian</label>
-                        <textarea class="form-control" rows="1" placeholder="Enter ..." disabled="disabled"></textarea>
-                      </div>
-                      <!-- Tanggal Kejadian -->
-                      <div class="form-group">
-                        <label>Tanggal Kejadian</label>
-                        <div class="input-group date">
-                          <div class="input-group-addon">
-                            <i class="fa fa-calendar"></i>
-                          </div>
-                          <input type="text" class="form-control pull-right" id="datepicker" disabled="disabled">
-                        </div>
-                        <!-- /.input group -->
-                      </div>
-                      <!-- /.form group -->
-                    </div>
-
-                    <div class="col-lg-6 col-md-6 col-sm-12">
-                      <h4>DATA KASUS</h4>
-                      <div class="form-group">
-                        <label>Kasus Posisi / Telaahan Masalah</label>
-                        <form>
-                          <textarea class="textarea" placeholder="Place some text here" style="width: 100%; height: 200px; font-size: 14px; line-height: 18px; border: 1px solid #dddddd; padding: 10px;"></textarea>
-                        </form>
-                      </div>
-                    </div>
-                  </div>
-                  <div class="row">
-                    <div class="col-lg-6 col-md-6 col-sm-12">
-                      <!-- Kesimpulan -->
-                      <div class="form-group">
-                        <label>Kesimpulan</label>
-                        <textarea class="form-control" rows="5" placeholder="Enter ..."></textarea>
-                      </div> 
-                    </div>
-                    <div class="col-lg-6 col-md-6 col-sm-12">
-                     <!-- Saran -->
-                      <div class="form-group">
-                        <label>Saran</label>
-                        <textarea class="form-control" rows="5" placeholder="Enter ..."></textarea>
-                      </div>
-                    </div>
-                  </div>
-                  <hr>
-              <!-- End CONTENT -->
-                  <div class="row">
-                    <div class="col-lg-4 col-md-4 col-sm-12">
-                      <h4>DATA Subyek</h4>
-                    <!-- Nama Lengkap -->
-                      <div class="form-group">
-                        <label>Nama Lengkap</label>
-                        <textarea class="form-control" rows="1" placeholder="Enter ..."></textarea>
-                      </div>
-                      <!-- Tempat Lahir -->
-                      <div class="form-group">
-                        <label>Tempat Lahir</label>
-                        <input type="text" class="form-control" placeholder="Enter ...">
-                      </div>
-                      <!-- Tanggal Lahir -->
-                      <div class="form-group">
-                        <label>Tanggal Lahir</label>
-                        <input type="date" class="form-control" placeholder="Enter ...">
-                      </div>
-                      <!-- Kebangsaan / Kewarganegaraan -->
-                      <div class="form-group">
-                        <label>Kebangsaan / Kewarganegaraan</label>
-                        <input type="text" class="form-control" placeholder="Enter ...">
-                      </div>
-                      <!-- Tempat Tinggal -->
-                      <div class="form-group">
-                        <label>Tempat Tinggal</label>
-                        <textarea class="form-control" rows="1" placeholder="Enter ..."></textarea>
-                      </div>
-                      <!-- Agama -->
-                      <div class="form-group">
-                        <label>Agama</label>
-                        <input type="text" class="form-control" placeholder="Enter ...">
-                      </div>
-                    </div>
-
-                    <div class="col-lg-4 col-md-4 col-sm-12">
-                      <h4>DATA PEKERJAAN</h4>
-                      <!-- Jabatan Resmi -->
-                      <div class="form-group">
-                        <label>Jabatan Resmi</label>
-                        <input type="text" class="form-control" placeholder="Enter ...">
-                      </div>
-                      <!-- Jabatan Lain -->
-                      <div class="form-group">
-                        <label>Jabatan Lain</label>
-                        <input type="text" class="form-control" placeholder="Enter ...">
-                      </div>
-                      <!-- Institusi / Lembaga -->
-                      <div class="form-group">
-                        <label>Institusi / Lembaga</label>
-                        <input type="text" class="form-control" placeholder="Enter ...">
-                      </div>
-                      <!-- Pendidikan -->
-                      <div class="form-group">
-                        <label>Pendidikan</label>
-                        <input type="text" class="form-control" placeholder="Enter ...">
-                      </div>
-                 <!-- Kategori Subyek -->
-                      <div class="form-group">
-                        <label>Kategori Subyek</label>
-                        <select class="form-control select2" style="width: 100%;">
-                        <option selected="selected">SWASTA</option>
-                          <option>PNS</option>
-                          <option>Hakim</option>
-                          <option>Pengacara</option>
-                          <option>Jaksa</option>
-                          <option>Gubernur</option>
-                          <option>Walikota</option>
-                        </select>
-                      </div>
-                        <!-- Kesimpulan -->
-                      <div class="form-group">
-                        <label>Keterangan</label>
-                        <textarea class="form-control" rows="5" placeholder="Enter ..."></textarea>
-                      </div> 
-                    </div> 
-
-                    <div class="col-lg-4 col-md-4 col-sm-12">
-                      <h4>DATA OBYEK</h4>
-                      <div class="form-group">
-                        <label>Benda Sitaan</label>
-                        <input type="Nilai" class="form-control" placeholder=" ">
-                        </div>
-                      <!-- Nilai Anggaran / Kontrak -->
-                      <div class="form-group">
-                        <label>Nilai Anggara / Kontrak</label>
-                        <input type="INT" class="form-control" placeholder=" ">
-                      </div>
-
-                      <!-- Kerugian Negarak -->
-                      <div class="form-group">
-                        <label>Kerugian Negara</label>
-                        <input type="INT" class="form-control" placeholder=" ">
-                      </div>
-                      <!-- Pemulihan Aset -->
-                      <div class="form-group">
-                        <label>Pemulihan Aset</label>
-                        <input type="INT" class="form-control" placeholder=" ">
-                      </div>
-
-                      <h4>DATA JAKSA</h4>
-                      <div class="form-group">
-                        <label>Jaksa Koordinator</label>
-                        <select class="form-control select2" >
-                          <option selected="selected">Riki Indera</option>
-                          <option>Akbar Permana</option>
-                          <option>Ade Pudjasari</option>
-                          <option>Tonny Soerojo</option>
-                          <option>Anastasia</option>
-                          <option>John Laksmana</option>
-                          <option>Shannine Davita</option>
-                        </select>
-                      </div>
-
-                      <div class="form-group">
-                        <label>Jaksa Anggota</label>
-                        <select class="form-control select2" multiple="multiple" data-placeholder="Pilih anggota...">
-                          <option>Akbar Permana</option>
-                          <option>Ade Pudjasari</option>
-                          <option>Tonny Soerojo</option>
-                          <option>Anastasia</option>
-                          <option>John Laksmana</option>
-                          <option>Akbar Permana</option>
-                          <option>Ade Pudjasari</option>
-                          <option>Tonny Soerojo</option>
-                          <option>Anastasia</option>
-                          <option>John Laksmana</option>
-                          <option>Shannine Davita</option>
-                          <option>Shannine Davita</option>
-                        </select>
-                      </div>
-                    </div>
-                  </div>
+  <div class="box-body">
+    <div class="row">
+      <div class="col-lg-6 col-md-6 col-sm-12">
+        <div class="row">
+          <div class="col-lg-6 col-md-6 col-sm-12">
+            <label>Status</label>
+            <div class="input-group">
+              <div class="input-group-btn"> 
+                <button type="button" class="btn btn-default" aria-label="stop" title="DIHENTIKAN"><span class="glyphicon glyphicon-minus-sign"></span> </button>
+                <button type="button" class="btn btn-default" aria-label="transfer" title="DIALIHKAN"><span class="glyphicon glyphicon-transfer"></span></button> </div> 
+                <input class="form-control" aria-label="Text input with multiple buttons" placeholder="Keterangan Status...">
               </div>
+          </div>
+          <div class="col-lg-6 col-md-6 col-sm-12">       
+              <!-- No. Surat -->
+            <div class="form-group">
+              <label> No. Surat</label>
+              <input type="text" class="form-control" placeholder="PRINT-">
+            </div>
+          </div>
+          <div class="col-lg-6 col-md-6 col-sm-12">
+            <div class="form-group">
+              <label>Pasal Disangkakan</label>
+              <select class="form-control" style="width: 250px;">
+                <option selected="selected">Pasal...</option>
+                <option>Pasal 1</option>
+                <option>Pasal 2</option>
+                <option>Pasal 3</option>
+                <option>Pasal 4</option>
+                <option>Pasal 5</option>
+                <option>Pasal 6</option>
+              </select>
+            </div>
+          </div>
+            <div class="col-lg-6 col-md-6 col-sm-12">
+            <!-- Tanggal Surat -->
+            <div class="form-group">
+              <label>Tanggal</label>
+              <div class="input-group date">
+                <div class="input-group-addon">
+                  <i class="fa fa-calendar"></i>
+                </div>
+                <input type="date" class="form-control pull-right" id="datepicker">
+              </div>
+              <!-- /.input group -->
+            </div>
+            <!-- /.form group -->
+              </div>              
+        </div>
+        <div class="panel panel-default">
+          <div class="panel-heading">
+            <h3 class="panel-title">Judul Kasus : Brunch 3 wolf moon tempor, sunt aliqua put a bird on it</h3>
+          </div>
+          <div class="panel-body">
+            <strong>Kasus Posisis: </strong> Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
+          </div>
+        </div>
+        <div class="list-group">
+          <a href="#" class="list-group-item">
+            <strong>PEMULIHAN ASET</strong>
+          </a>
+          <a href="#" class="list-group-item">Nilai Anggaran <div class="pull-right">
+            Rp. xxx.xxx.xxx.xxx 
+          </div></a>
+          <a href="#" class="list-group-item">Kerugian Negara<div class="pull-right">
+            Rp. xxx.xxx.xxx.xxx 
+          </div></a>
+          <a href="#" class="list-group-item">Pemulihan Aset<div class="pull-right">
+            Rp. xxx.xxx.xxx.xxx 
+          </div></a>
+        </div>
+      </div>
+      <div class="col-lg-6 col-md-68 col-sm-12">
+        <!-- Widget: user widget style 1 -->
+        <div class="box box-widget widget-user-2 bg-gray">
+          <!-- Add the bg color to the header using any of the bg-* classes -->
+          <div class="widget-user-header">
+            <div class="widget-user-image">
+              <img class="img-circle" src="<?= base_url() ?>assets/admin/img/steven.jpg" alt="User Avatar">
+            </div>
+            <!-- /.widget-user-image -->
+            <h3 class="widget-user-username">Steven Carmichael</h3>
+            <h5 class="widget-user-desc">DITAHAN</h5>
+          </div>
+          <div class="box-footer no-padding">
+            <ul class="nav nav-stacked">
+              <li><a href="#">Surat Penetapan Tersangka <strong class="pull-right">B-xxx/xx/mm/yyyy</strong></a></li>
+              <li><a href="#">Institusi / Lembaga <span class="pull-right">Pem.Prov Lampung</span></a></li>
+              <li><a href="#">Jabatan Resmi <span class="pull-right ">Gubernur</span></a></li>
+              <li><a href="#">Jabatan Lain <span class="pull-right">Panitia Lelang</span></a></li>
+            </ul>
+          </div>
+        </div>
+        <!-- /.widget-user -->
+
+      </div>
+      <div class="box-body">
+        <div class="col-lg-6 col-md-6 col-sm-12">
+          <h4>DATA Pengacara</h4>
+          <div class="form-group">
+            <label>Pengacara Koordinator</label>
+            <select class="form-control" >
+              <option selected="selected">Riki Indera</option>
+              <option>Akbar Permana</option>
+              <option>Ade Pudjasari</option>
+              <option>Tonny Soerojo</option>
+              <option>Anastasia</option>
+              <option>John Laksmana</option>
+              <option>Shannine Davita</option>
+            </select>
+          </div>
+
+          <!-- /.col -->
+
+          <div class="form-group">
+            <label>Pengacara Anggota</label>
+            <select class="form-control select2" multiple="multiple" data-placeholder="Pilih anggota...">
+              <option>Akbar Permana</option>
+              <option>Ade Pudjasari</option>
+              <option>Tonny Soerojo</option>
+              <option>Anastasia</option>
+              <option>John Laksmana</option>
+              <option>Akbar Permana</option>
+              <option>Ade Pudjasari</option>
+              <option>Tonny Soerojo</option>
+              <option>Anastasia</option>
+              <option>John Laksmana</option>
+              <option>Shannine Davita</option>
+              <option>Shannine Davita</option>
+            </select>
+          </div>
+        </div>
+      </div>
               <div class="box-footer">
                 <a href="<?= base_url() ?>admin/register/rp3sus" class="btn btn-danger"> BATAL</a>
                 <div class="box-tools pull-right">
